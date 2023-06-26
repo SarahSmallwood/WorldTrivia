296f9b7cca3496d7c9c16ce496d7afec45b066a2
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-06-26 12:00:43 -0400

    Wireframe

---
+++ app.js
@@ -0,0 +1,100 @@
+// Globe Icon 
+// Press to start game
+
+//Game Space 
+// Location Info
+//Level One
+//Seven Continents so seven rounds
+
+//Round One
+// Antarctica
+// Background Image change to Ice
+// Button Start Round
+
+//Ice Everywhere!! You are in danger of freezing to death
+// Only your survival gear will save you
+//Answer World Trivia to access survival backpack
+
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+//Next Round
+
+//Round Two
+//South America
+//Background Image change to Amazon
+//Button Start Round
+
+//Lost in the Jungle
+//The trail is too thick for you to see through the jungle to find your way. You keep becoming trapped in the vines!
+//Answer World Trivia to access survival backpack
+
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+//Next Round
+
+//Round Three
+//North America
+//Grand Canyon Flood
+//A hike through America's greatest wonder has turned to disaster. A brutal rainstorm has filled some of the canyon and turned to a flash flood.
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+//Next Round
+
+
+//Round Four
+//Africa
+//You've explored Africa from Mt. Kilamanjaro to Cape Town South Africa, but there is one pest you cant escape from
+// MOSQUITOS!!!
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+//Next Round
+
+//Round Five
+//Asia
+//Let's try extreme tourism! How about you visit the highest point on Earth, Mt. Everest!
+//The higher up you go, the harder it is to breathe
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+
+//Next Round
+
+//Round Six
+//Australia
+//Adventures on the Outback
+// Crikey!! A snake bite!! 
+// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
+
+//Next Round
+
+//Round Seven
+//Europe
+//Paris is beautiful, but climbing the Eiffel Tower was not your best idea.
+//// Trivia Box
+// Question
+// three answer selections
+// if wrong answers, game over
+//correct answer then next round
