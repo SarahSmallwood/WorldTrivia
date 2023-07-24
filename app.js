467e2ce9718f899ecdc0b2e9c8094ed023bdf6ba
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-07-23 22:51:39 -0400

    quiz answer setup finish

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ const questionList = {
     }
 }
 
+// Function to show game directions
+
 function showDirections(){
     let howtoPlay = document.querySelector(".directions");
     if(howtoPlay.style.display == 'none'){
@@ -52,6 +54,8 @@ function showDirections(){
         howtoPlay.style.display = 'none';
     }
 };
+
+//function to show quiz
 
 function showQuiz(selector) {
     let element = document.querySelector(selector);
@@ -64,61 +68,77 @@ function showQuiz(selector) {
 }
 
 
+// Quiz answer Data in array 
+// correct answer is underneath each quiz answer set 
+// and will be called upon in later function to keep score of the quiz
+const quizAnswers = [
+    {
+        a1: {
+           0: "50",
+           1: '20',
+           2:'90',
+           3:'70'
+        },
+        correctAnswer: 2
+    },
+    {
+        a2: {
+           0: "Andes Mountains", 
+           1: "Patagonia Chile", 
+           2:"Lake Titicaka", 
+           3:"Amazon Rainforest"    
+        },
+        correctAnswer: 3
+    },
+    {
+        a3: {
+            0: "Death Valley", 
+            1: "Mexico City",
+            2: "New Orleans, Lousiana",
+            3: "Great Salt Lake"
+        },
+        correctAnswer: 0
+
+    },
+    {
+        a4: {
+            0: "Zambezi River",
+            1: "Nile River",
+            2: "Snake River",
+            3: "Congo River"
+        },
+        correctAnswer: 1
+    },
+    {
+        a5: {
+            0: "The Outback",
+            1: "The Bush",
+            2: "Great Barrier Reef",
+            3: "Australian Alps" 
+        },
+        correctAnswer: 2
+    },
+    {
+        a6: {
+            0: "China",
+            1: "India",
+            2: "Vietnam",
+            3: "Bangladesh" 
+        },
+        correctAnswer: 1
+    },
+    {
+        a7: {
+            0: "Stone Henge",
+            1: "The Louvre",
+            2: "Eiffel Tower",
+            3: "Rome Colusseum", 
+        },
+        correctAnswer: 1
+    }
+]
 
 
-let correctAnswerList = [2, 3, 0, 1, 2, 1, 1]
-let answerList =
-    [
-        // Antartica answers
-        [
-            "50",
-            '20',
-            '90',
-            '70'
-        ],
-        // South America answers
-        [
-            "Andes Mountains", 
-            "Patagonia Chile", 
-            "Lake Titicaka", 
-            "Amazon Rainforest"
-        ],
-        // NA Answers 
-        [
-            "Death Valley", 
-            "Mexico City",
-            "New Orleans, Lousiana",
-            "Great Salt Lake", 0
-        ],
-        // Africa Answers
-        [
-            "Zambezi River",
-            "Nile River",
-            "Snake River",
-            "Congo River", 1
-        ],
-        // Australia Answers
-        [
-            "The Outback",
-            "The Bush",
-            "Great Barrier Reef",
-            "Australian Alps", 2  
-        ],
-        // Asia Answers
-        [
-            "China",
-            "India",
-            "Vietnam",
-            "Bangladesh", 1
-        ],
-        // europe answers
-        [
-            "Stone Henge",
-            "The Louvre",
-            "Eiffel Tower",
-            "Rome Colusseum", 1
-        ]
-    ]
 
 function showQuestion(selector) {
 
@@ -152,6 +172,8 @@ function checkAnswer(selector) {
 
 }
 
+// Calling function to show questions
+
 showQuestion('.antarcticaQ');
 showQuestion('.southAQuiz');
 showQuestion('.northAQuiz');
@@ -163,84 +185,7 @@ showQuestion('.europeQuiz');
 
 
 
-
-
-// let q2 = {
-//     title2: "The world's largest source of oxygen comes from where in South America?",
-//     a2:[{ text: "Andes Mountains", isCorrect: false },
-//     { text: "Patagonia Chile", isCorrect: false },
-//     { text: "Lake Titicaca", isCorrect: false },
-//     { text: "Amazon Rainforest", isCorrect: true }
-//     ]
-// }
-
-// TODO - could be deleted
-
-//   showQuestion2();
-
-
-// let q3 = {
-//     title3: "What is considered the lowest geographical point in North America?",
-//     a3: [{ text: "Death Valley", isCorrect: true },
-//     { text: "Mexico City", isCorrect: false },
-//     { text: "New Orleans, Lousiana", isCorrect: false },
-//     { text: "Great Salt Lake", isCorrect: false }
-//     ]  
-// }
-
-// TODO - could be deleted
-
-
-
-// let q4 = {
-//     title4: "Which river in Africa is the planet's longest?",
-//     a4: [{ text: "Zambezi River", isCorrect: false },
-//     { text: "Nile River", isCorrect: true },
-//     { text: "Snake River", isCorrect: false },
-//     { text: "Congo River", isCorrect: false }
-//     ]  
-// }
-
-// TODO - could be deleted
-
-
-
-// let q5 = {
-//     title5: [{ text: "The Outback", isCorrect: false },
-//     { text: "The Bush", isCorrect: false },
-//     { text: "Great Barrier Reef", isCorrect: true },
-//     { text: "Australian Alps", isCorrect: false }
-//     ]
-// } 
-
-// TODO - could be deleted
-
-
-
-// let q6 = {
-//     title6: "Asia holds the largest percentage of the world's population. Which Asian country has the most people?",
-//     a5: [{ button: "China", isCorrect: false },
-//     { text: "India", isCorrect: true },
-//     { text: "Vietnam", isCorrect: false },
-//     { text: "Bangladesh", isCorrect: false }
-//     ]  
-// } 
-
-
-// TODO - could be deleted
-
-
-
-
-// let q7 = {
-//     title7: "The most visited attraction in Europe is ______________",
-//     a7: [{ text: "Stone Henge", isCorrect: false },
-//     { text: "The Louvre", isCorrect: true },
-//     { text: "Eiffel Tower", isCorrect: false },
-//     { text: "Rome Colusseum", isCorrect: false }
-//     ] 
-// }
-
+// 
 
 
 // Answer options for questions functionality
